1fcf11b474109dbc59bdda5db6eb26b60a9f95a5
Joe Somma <jsomma@millentek.com>
2015-11-13 23:30:08 -0500

    Contacts table export for production

---
+++ portfolio/sql/joesomma_udemy_export_contacts_prod.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 13, 2015 at 11:19 PM
+-- Server version: 5.5.44-MariaDB-cll-lve
+-- PHP Version: 5.4.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `joesomma_udemy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `Contacts` (
+  `ContactId` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Message` text NOT NULL,
+  `AdminNotified` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ContactId`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table holds messages sent from users on joesomma.me portfolio web page' AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
